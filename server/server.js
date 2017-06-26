0deecff7969531dfea7eed174a6c573b822bee71
Ido Avidan <IDOA@NIPZWE.ZW>
2017-06-26 14:41:32 +0200

    express-graphql server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import schema from './schema';
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
+app.listen(4000);
