b7f712e3e71ed394c6a3ff5b3b31d9032392632f
Ido Avidan <IDOA@NIPZWE.ZW>
2017-06-26 15:51:55 +0200

    fuck comment

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
-import schema from './schema';
+// import schema from './schema';
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 import mongoose from 'mongoose';
+import {buildSchema} from 'graphql';
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
   schema,
-  graphiql: true,
   pretty : true
 }));
 
