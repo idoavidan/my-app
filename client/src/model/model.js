8ff06330ce0231f610ba668094b8758fc7b2c057
Ido Avidan <IDOA@NIPZWE.ZW>
2017-06-27 09:36:01 +0200

    no model yet

--- client/src/model/model.js
+++ client/src/model/model.js
@@ -1,11 +1,21 @@
-import Lokka from 'lokka';
-import Transport from 'lokka-transport-http';
+// import Lokka from 'lokka';
+// import Transport from 'lokka-transport-http';
+//
+// const Client  = new Lokka({
+//       transport: new Transport('http://localhost:8080/graphql')
+// });
+//
+// let dataPromise = Client.query('{hello}');
+// .then(res => console.log(res));
 
-const Client  = new Lokka({
-      transport: new Transport('http://localhost:8080/graphql')
-});
-
-let dataPromise = Client.query('{hello}').then(res => console.log(res));
+// import HttpTransport from 'lokka-transport-http';
+//
+// const transport = new HttpTransport('http://localhost:8080/graphql/');
+// transport.send(`
+//   {hello}
+// `).then(res => {
+//     console.log(res);
+// });
 
 export const getAll = function(){
   return "dataPromise";
