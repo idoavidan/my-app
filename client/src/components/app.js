0a6480a8928b2079fbc0c1f2b334ffcb6e2f3139
Ido Avidan <IDOA@NIPZWE.ZW>
2017-06-28 16:15:49 +0200

    no more hardcoded pics

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,33 +1,14 @@
 import ImgBox from './imgBox';
 import React from 'react';
 
-
-const init = {pics: [
-                      {url :'https://unsplash.it/200/200/?random',
-                       likes :11,
-                       comments : ["hahaha met"], title : "Banana"},
-                      {url :'https://unsplash.it/200/200/?random',
-                       likes :2,
-                       comments : [], title : "Falafel"},
-                      {url :'https://unsplash.it/200/200/?random',
-                       likes :3,
-                       comments : [], title : "Pizza"},
-                      {url :'https://unsplash.it/200/200/?random',
-                       likes :2,
-                       comments : [],
-                       title : "House"},
-                      {url :'https://unsplash.it/200/200/?random',
-                        likes :2123,
-                      comments : [], title : "Food"}
-                     ]}
-
-const imgBoxes = init.pics.map((key,index) =>
-(<ImgBox url = {key.url} likes = {key.likes} comments = {key.comments} title = {key.title} key={index}/>));
-
+const func = function(initValue){
+  return initValue.map((key,index) =>
+      (<ImgBox url = {key.url} likes = {key.likes} comments = {key.comments} title = {key.title} key={index}/>));
+}
 const App = (props, context) => (
   <div >
     <h3 style={{textAlign: 'center',backgroundColor: 'grey'}}>{"props.fuck().then({return})"}</h3>
-    {imgBoxes}
+    {func(props.initValue)}
   </div>
 );
 
