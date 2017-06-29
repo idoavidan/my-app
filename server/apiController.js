a078aff0313d2114ab63954ebee69f44cb79c9ab
Ido Avidan <IDOA@NIPZWE.ZW>
2017-06-29 11:08:14 +0200

    changed .js structure

---
+++ server/apiController.js
@@ -0,0 +1,37 @@
+var JsonDB = require('node-json-db');
+
+import {buildSchema} from 'graphql';
+
+//schema
+export const schema = buildSchema(`
+  type PicPost {
+    url: String
+    likes: Int
+    comments : [String]
+    title : String
+  }
+  type pics {
+    pics : [PicPost]
+  }
+  type Query {
+    hello: String
+    getLastPicPost: PicPost
+    getPics: pics
+  }
+`);
+
+// "db" connection
+var db = new JsonDB("myDB", true, false);
+
+// The root provides a resolver function for each API endpoint
+export const root = {
+  hello: () => {
+    return "Init";
+  },
+  getLastPicPost: () => {
+    return db.getData("/testDB").pics[0];
+  },
+  getPics: () => {
+    return db.getData("/testDB");
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,41 +1,11 @@
 // import schema from './schema';
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-var JsonDB = require('node-json-db');
-import {buildSchema} from 'graphql';
+import {root, schema} from './apiController';
+
 
 // db
-var db = new JsonDB("myDB", true, false);
 
-var schema = buildSchema(`
-  type PicPost {
-    url: String
-    likes: Int
-    comments : [String]
-    title : String
-  }
-  type pics {
-    pics : [PicPost]
-  }
-  type Query {
-    hello: String
-    getLastPicPost: PicPost
-    getPics: pics
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return "Init";
-  },
-  getLastPicPost: () => {
-    return db.getData("/testDB").pics[0];
-  },
-  getPics: () => {
-    return db.getData("/testDB");
-  }
-};
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
