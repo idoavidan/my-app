48f04da78c2966f60c4a1a9531090d4c1956ee59
Ido Avidan <IDOA@NIPZWE.ZW>
2017-06-29 10:57:40 +0200

    changed function format

--- client/src/model/model.js
+++ client/src/model/model.js
@@ -4,13 +4,6 @@ import Transport from 'lokka-transport-http';
 const Client  = new Lokka({
     transport: new Transport('/graphql')
 });
-export const helloPromise = async function(x = Client.query('{hello}')){
-    return x;
-}
 
-export const initPromise = async function(x = Client.query('{getPics{pics{url,likes,comments,title}}}')){
-    return x;
-}
-
-
-// const ido =
+export const initPromise =
+    async () => Client.query('{getPics{pics{url,likes,comments,title}}}');
